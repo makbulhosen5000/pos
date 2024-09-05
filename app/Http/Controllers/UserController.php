fd376c34df31afa2cf43b9ca21324140606fd1f1
makbulhosen5000 <makbulhosen5000@gmail.com>
2024-09-05 22:01:35 +0600

    fet:added otp mail notification

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Helper\JWTToken;
+use App\Mail\OTPMail;
 use App\Models\User;
 use Exception;
 use Firebase\JWT\JWT;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
@@ -57,6 +59,27 @@ class UserController extends Controller
                 'message' => 'Unauthorized'
             ],200);
         }
-
+    
+    }
+    public function sentOtpCode(Request $request){
+        $email = $request->input('email');
+        $otp = rand(1000,9999);
+        $count = User::where('email', '=', $email)->count();
+        
+        if($count == 1){
+            // otp send to email 
+            Mail::to($email)->send(new OTPMail($otp));
+            //otp code update in database table
+            User::where('email','=', $email)->update(['otp'=>$otp]);
+            return response()->json([
+                'status'  =>  'success',
+                'message' => '4 digit otp code has been sent to your mail!'
+            ],200);
+        }else{
+            return response()->json([
+                'status'  =>  'failed',
+                'message' => 'Unauthorized'
+            ],200);
+        }
     }
 }
