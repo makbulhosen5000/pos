0e32ced0f9caa72c903d25bc941c48a675f16fcd
makbulhosen5000 <makbulhosen5000@gmail.com>
2024-09-11 22:56:09 +0600

    fet:added registration

--- resources/views/components/auth/registration-form.blade.php
+++ resources/views/components/auth/registration-form.blade.php
@@ -44,25 +44,60 @@
 
   async function onRegistration() {
 
+            let email = document.getElementById('email').value;
+            let fastName = document.getElementById('fastName').value;
+            let lastName = document.getElementById('lastName').value;
+            let mobile = document.getElementById('mobile').value;
+            let password = document.getElementById('password').value;
 
-
-            let postBody={
-                "firstName":document.getElementById('firstName').value,
-                "lastName":document.getElementById('lastName').value,
-                "email":document.getElementById('email').value,
-                "password":document.getElementById('password').value,
-                "mobile":document.getElementById('mobile').value,
+            if(email.length === 0){
+                errorToast('Email is required');
+            }else if(fastName.length === 0){
+                errorToast('First Name is requered');
+            }else if(lastName.length === 0){
+                errorToast('Last Name is requered');
+            }else if(mobile.length === 0){
+                errorToast('Mobile number is requerid');
+            }else if(password.length === 0){
+                errorToast('Password is required');
+            }else{
+                showLoader();
+                let res = await axios.post('/userRegistration',{
+                    email:email,
+                    fastName:fastName,
+                    lastName:lastName,
+                    email:email,
+                    password:password,
+                })
+                if(res.status = 200  && res.data['status'] === 'success'){
+                    successToast(res.data['message']);
+                    setTimeOut(function() {
+                        window.loaction.href('/login')
+                    },2000)
+                }else{
+                    errorToast(res.data['message']);
+                }
             }
+            
 
-              showLoader();
-              let res=await axios.post("/user-registration",postBody);
-              hideLoader()
-              if(res.status===200 && res.data['status']==='success'){
-                  window.location.href="/userLogin";
-              }
-              else{
-                  errorToast(res.data['message']);
-              }
+
+            // let postBody={
+            //     "firstName":document.getElementById('firstName').value,
+            //     "lastName":document.getElementById('lastName').value,
+            //     "email":document.getElementById('email').value,
+            //     "password":document.getElementById('password').value,
+            //     "mobile":document.getElementById('mobile').value,
+            // }
+
+            //   showLoader();
+            //   let res=await axios.post("/user-registration",postBody);
+            //   hideLoader()
+            //   if(res.status===200 && res.data['status']==='success'){
+            //       window.location.href="/userLogin";
+            //   }
+            //   else{
+            //       errorToast(res.data['message']);
+            //   }
 
   }
 
